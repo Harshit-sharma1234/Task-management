34142ecb54e0a051352da6266b8fe919b71818b0
Harshit Sharma <harshitmrsharma@gmail.com>
2026-03-26 13:54:13 +0530

    refactor: Migrate project to Next.js App Router with TypeScript and move frontend to root.

--- src/app/signup/actions.ts
+++ src/app/signup/actions.ts
@@ -2,24 +2,27 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/lib/supabase/server'
 
-export async function signup(formData) {
+export async function signup(formData: FormData) {
   const supabase = await createClient()
 
   const fullName = formData.get('fullName')
   
   const data = {
-    email: formData.get('email'),
-    password: formData.get('password'),
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
     options: {
         data: {
-            full_name: fullName
+            full_name: fullName,
+            name: fullName
         }
     }
   }
+  console.log("data", data)
 
   const { error } = await supabase.auth.signUp(data)
+  console.log("Error", error)
 
   if (error) {
     redirect('/signup?message=Could not create user: ' + error.message)
