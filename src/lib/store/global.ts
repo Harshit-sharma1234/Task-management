82b26325dc312ff694f6d91f636e95c9bdd9f81b
Harshit Sharma <harshitmrsharma@gmail.com>
2026-04-22 17:16:47 +0530

    feat: add issues state and management actions to global store

--- src/lib/store/global.ts
+++ src/lib/store/global.ts
@@ -3,14 +3,18 @@ import { create } from 'zustand';
 interface GlobalState {
   projects: any[];
   team: any[];
+  issues: any[];
   activeWorkspaceId: string | null;
   isInitialLoadComplete: boolean;
   
   setProjects: (projects: any[]) => void;
   setTeam: (team: any[]) => void;
+  setIssues: (issues: any[]) => void;
   setActiveWorkspaceId: (id: string | null) => void;
   setInitialLoadComplete: (val: boolean) => void;
   
+  addIssue: (issue: any) => void;
+  removeIssue: (id: string) => void;
   updateProject: (project: any) => void;
   updateTeamMember: (member: any) => void;
 }
@@ -18,13 +22,23 @@ interface GlobalState {
 export const useGlobalStore = create<GlobalState>((set) => ({
   projects: [],
   team: [],
+  issues: [],
   activeWorkspaceId: null,
   isInitialLoadComplete: false,
 
   setProjects: (projects) => set({ projects }),
   setTeam: (team) => set({ team }),
+  setIssues: (issues) => set({ issues }),
   setActiveWorkspaceId: (id) => set({ activeWorkspaceId: id }),
   setInitialLoadComplete: (val) => set({ isInitialLoadComplete: val }),
+
+  addIssue: (issue) => set((state) => ({ 
+    issues: [issue, ...state.issues] 
+  })),
+
+  removeIssue: (id) => set((state) => ({ 
+    issues: state.issues.filter(i => i.id !== id) 
+  })),
 
   updateProject: (updatedProject) => set((state) => ({
     projects: state.projects.map(p => p.id === updatedProject.id ? { ...p, ...updatedProject } : p)
