c8dcd1f33d274e4fb1f97935adc48614647bbc41
Harshit Sharma <harshitmrsharma@gmail.com>
2026-04-29 14:12:53 +0530

    chore: disable @next/next/no-html-link-for-pages lint rule in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "react-hooks/exhaustive-deps": "off",
       "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off"
